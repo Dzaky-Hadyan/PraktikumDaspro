23995614c210b35e3dad29414b996b95cbbce80a
Dzaky Hadyan Eliyanta <dzakyhadyaneliyanta06@gmail.com>
2024-09-26 09:00:40 +0700

    update ternary operator

--- Praktikum05/PemilihanBilangan07.java
+++ Praktikum05/PemilihanBilangan07.java
@@ -4,14 +4,9 @@ public class PemilihanBilangan07 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String ganjilGenap;
+        ganjilGenap = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.println(ganjilGenap);
         sc.close();
     }
 }
